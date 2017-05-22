c9594c22c9b834a658b37d93af7d0614d6ade2a6
Travis <support@sitepen.com>
2017-05-22 23:07:55 +0000

    Published by Travis from commit d528b20709633486b403263d427df8063bf7c434

--- tests/unit/all.js
+++ tests/unit/all.js
@@ -4,13 +4,14 @@
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./src/log", "./src/commands/getReleases", "./src/util/crypto", "./src/util/environment", "./src/util/file", "./src/util/Git", "./src/util/GitHub", "./src/util/streams", "./tasks/util/getGithubSlug"], factory);
+        define(["require", "exports", "./src/log", "./src/commands/getReleases", "./src/commands/installDependencies", "./src/util/crypto", "./src/util/environment", "./src/util/file", "./src/util/Git", "./src/util/GitHub", "./src/util/streams", "./tasks/util/getGithubSlug"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     require("./src/log");
     require("./src/commands/getReleases");
+    require("./src/commands/installDependencies");
     require("./src/util/crypto");
     require("./src/util/environment");
     require("./src/util/file");
