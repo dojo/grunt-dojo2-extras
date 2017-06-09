b415b6eb0e351953157d34e01c2156470d7d4f45
Travis <support@sitepen.com>
2017-06-09 01:21:02 +0000

    Published by Travis from commit e318275bb5b2989d91c6b3263c9f254ac456674b

--- tests/unit/all.js
+++ tests/unit/all.js
@@ -4,12 +4,13 @@
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./src/log", "./src/commands/getReleases", "./src/commands/initialize/initAuthorization", "./src/commands/initialize/createDeployKey", "./src/commands/typedoc", "./src/commands/publish", "./src/commands/installDependencies", "./src/commands/sync", "./src/util/crypto", "./src/util/environment", "./src/util/file", "./src/util/Git", "./src/util/process", "./src/util/GitHub", "./src/util/Travis", "./src/util/streams", "./tasks/util/getGithubSlug"], factory);
+        define(["require", "exports", "./src/log", "./src/commands/decryptDeployKey", "./src/commands/getReleases", "./src/commands/initialize/initAuthorization", "./src/commands/initialize/createDeployKey", "./src/commands/typedoc", "./src/commands/publish", "./src/commands/installDependencies", "./src/commands/sync", "./src/util/crypto", "./src/util/environment", "./src/util/file", "./src/util/Git", "./src/util/process", "./src/util/GitHub", "./src/util/Travis", "./src/util/streams", "./tasks/util/getGithubSlug"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     require("./src/log");
+    require("./src/commands/decryptDeployKey");
     require("./src/commands/getReleases");
     require("./src/commands/initialize/initAuthorization");
     require("./src/commands/initialize/createDeployKey");
